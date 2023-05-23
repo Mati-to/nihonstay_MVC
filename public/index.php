1f9a0f48ceb97c67c08cdd1b399f6e7c160f693e
Matías Alfaro <matiasalfaro2.a@gmail.com>
2023-05-23 15:12:16 -0400

    Crud properties done. Starting with landlords Crud

--- public/index.php
+++ public/index.php
@@ -3,15 +3,24 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\PropsController;
+use Controllers\LandController;
 
 $router = new Router();
 
+// Property routes
 $router->get('/admin', [PropsController::class, 'index']);
 $router->get('/properties/create', [PropsController::class, 'create']);
 $router->post('/properties/create', [PropsController::class, 'create']);
 $router->get('/properties/update', [PropsController::class, 'update']);
 $router->post('/properties/update', [PropsController::class, 'update']);
 $router->post('/properties/delete', [PropsController::class, 'delete']);
+
+// Landlord routes
+$router->get('/landlords/create', [LandController::class, 'create']);
+$router->post('/landlords/create', [LandController::class, 'create']);
+$router->get('/landlords/update', [LandController::class, 'update']);
+$router->post('/landlords/update', [LandController::class, 'update']);
+$router->post('/landlords/delete', [LandController::class, 'delete']);
 
 $router->checkRoutes();
 
