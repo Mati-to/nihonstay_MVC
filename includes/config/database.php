59c2419a625ed35b8d758b57eff4966ab20633e2
Matías Alfaro <matiasalfaro2.a@gmail.com>
2023-06-11 14:55:41 -0400

    Connection to DB done

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,13 +1,19 @@
 <?php
 
-function connectionDB() : mysqli {
-   $db = new mysqli('localhost', 'root', 'matito3593', 'nihonstay_crud');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-   if(!$db){
+function connectionDB(): mysqli
+{
+   $dbHost = $_ENV['DB_HOST'];
+   $dbUsername = $_ENV['DB_USER'];
+   $dbPassword = $_ENV['DB_PASSWORD'];
+   $dbName = $_ENV['DB_NAME'];
+   $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+   if (!$db) {
       echo 'Connection to database failed';
       exit;
-   } 
+   }
    return $db;
 }
-
-
